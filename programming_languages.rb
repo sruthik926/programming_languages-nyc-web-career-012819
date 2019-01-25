0f4ee5684fa60baffe313c106b22f8d5cc37d848
Sruthi Krishna <sruthik926@gmail.com>
2019-01-25 00:58:44 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,7 +7,7 @@ def reformat_languages(languages)
      #binding.pry
     lang.each do |k, v|
      v.each do |key, val|
-       if 
+       if k = 
        new_hash[k] = {key => val,
                     :style => [lang_type]
       }
